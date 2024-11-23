18042ff1637b4ba1ba9167c7174f12f6c10a5dce
Carlos Eduardo <142689457+carlosfernandino@users.noreply.github.com>
2024-11-23 22:40:41 +0000

    Feat - Instalação do react-router-dom
    - Instalação do router para melhorar navegação da API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import "./styles.css";
 import { ListaLivrosView } from "./livro/ListaLivrosView";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BuscarLivrosView } from "./livro/BuscarLivrosView";
 
 export default function App() {
-  return <ListaLivrosView />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element= {<ListaLivrosView/>}></Route>
+        <Route path="/busca" element= {<BuscarLivrosView/>}></Route>
+      </Routes>
+    </Router>
+  );
 }
---
+++ src/livro/BuscarLivrosView.js
@@ -0,0 +1,7 @@
+export function BuscarLivrosView() {
+  return (
+    <div>
+      <h1>Teste</h1>
+    </div>
+  );
+}
