252be308a06ea4b8c34097259dc971ae338aadaa
Carlos Eduardo <142689457+carlosfernandino@users.noreply.github.com>
2024-11-24 01:38:54 +0000

    feat: Adiciona página de detalhes do livro
    - Criação da rota '/livro/:id' para exibir detalhes do livro ao clicar no card
    - Modificação do componente Card para incluir link para a página de detalhes
    - Criação do componente LivroDetalhesView para exibir informações detalhadas do livro
    - Utilização do React Router para navegação entre a lista de livros e a página de detalhes
    - Exibição de informações como título, autor, imagem e descrição do livro

--- src/livro/Card.js
+++ src/livro/Card.js
@@ -1,27 +1,28 @@
+import { Link } from "react-router-dom";
 import "./Card.css";
 
-export function Card({ title, author, image, onMove }) {
+export function Card({ id, title, author, image, onMove }) {
   return (
     <div className="card">
-      <img
-        src={
-          image || "https://via.placeholder.com/150"
-        } /* Placeholder se não houver imagem */
-        alt={`Capa do livro ${title}`}
-        className="card-image"
-      />
-      <div className="card-content">
-        <h3>{title}</h3>
-        {author && <p className="card-author">por {author}</p>}
-        <select onChange={(e) => onMove(e.target.value)} defaultValue="">
-          <option value="" disabled>
-            Mover para...
-          </option>
-          <option value="estouLendo">Estou lendo</option>
-          <option value="jaLi">Já li</option>
-          <option value="queroLer">Quero ler</option>
-        </select>
-      </div>
+      <Link to={`/livro/${id}`}>
+        <img
+          src={image || "https://via.placeholder.com/150"}
+          alt={`Capa do livro ${title}`}
+          className="card-image"
+        />
+        <div className="card-content">
+          <h3>{title}</h3>
+          {author && <p className="card-author">por {author}</p>}
+        </div>
+      </Link>
+      <select onChange={(e) => onMove(e.target.value)} defaultValue="">
+        <option value="" disabled>
+          Mover para...
+        </option>
+        <option value="estouLendo">Estou lendo</option>
+        <option value="jaLi">Já li</option>
+        <option value="queroLer">Quero ler</option>
+      </select>
     </div>
   );
 }
